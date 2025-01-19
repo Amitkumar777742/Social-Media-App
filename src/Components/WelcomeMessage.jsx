57dec928083092f679eec9483a43d079f047884c
Amit Singh <amitsingh@Amits-MacBook-Air.local>
2025-01-20 04:31:13 +0530

    Resolved merge conflicts

---
+++ src/Components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WelcomeMessage = ({ onGetPostsClick }) => {
+  return (
+    <center className="welcome-message">
+      <h1>There are No post</h1>
+      
+    </center>
+  );
+};
+
+export default WelcomeMessage;
